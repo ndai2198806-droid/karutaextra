2d9d444d86f8dbfa0e73bc1898949d2fd9ab6fe3
ndai2198806-droid <ndai2198806@gmail.com>
2025-10-20 13:53:18 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -9,9 +9,10 @@ def home():
     return "Bot đang hoạt động!"
 
 def run():
-    port = int(os.environ.get("PORT", 8080))  # Lấy PORT từ Render hoặc dùng mặc định 8080
+    port = int(os.environ.get("PORT", 5000))  # Lấy PORT từ Render hoặc dùng mặc định 8080
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
     t.start()
+
